13ffd09b3ed80a37c6b5ce9f4e98f51f3ef279f8
SinoxMayer <arda_kaptan@hotmail.com>
2020-06-19 01:41:49 +0300

    ilk hali
    ilk halii

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]private float _moveSpeed = 1f;
+    [SerializeField]private float _jumpSpeed = 2f;
+    
+    private Rigidbody _rigidbody;
+
+    private bool _isGrounded;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();    
+    }
+
+
+
+    private void CheckInput()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+           Move(Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+ 
+        
+    }
+
+    private void Move(Vector3 direction)
+    {
+        _rigidbody.AddForce(direction * _moveSpeed,ForceMode.Acceleration);
+    }
+
+    private void Jump()
+    {
+
+        if (!_isGrounded)
+        {
+            return;
+        }
+        _rigidbody.AddForce(Vector3.up * (_jumpSpeed ),ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        _isGrounded = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Collectible collectible = other.GetComponent<Collectible>();
+        bool isCollectible = collectible != null;
+        if (isCollectible)
+        {
+            collectible.Collect();
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFallow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFallow : MonoBehaviour
+{
+   [SerializeField] private Transform target;
+   [SerializeField] private float smoothness = 1f;
+   [SerializeField] private Vector3 offset;
+   private bool _isTargetNull;
+
+   // Start is called before the first frame update
+    void Start()
+    {
+        _isTargetNull = target == null;
+    }
+
+    private void LateUpdate()
+    {
+        if (_isTargetNull)
+        {
+            return;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, b: target.position + offset, Time.deltaTime * smoothness);
+    }
+}
