421b73f21f1c0fd7d250a732ebb15f057e794c30
SinoxMayer <arda_kaptan@hotmail.com>
2020-06-29 22:35:14 +0300

    321
    312

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void ChangeScene()
+    {
+
+        StartCoroutine(ChangeSceneCoroutine());
+    }
+    
+    private IEnumerator ChangeSceneCoroutine()
+    {
+        
+        yield return  new  WaitForSeconds(1f);
+
+        LoadScene(GetActiveScene());
+    }
+
+    public void NextLevel()
+    {
+        LoadScene(GetActiveScene()+ 1);
+    }
+
+    private void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    private int GetActiveScene()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+}
